dec21abcb397bb91c204da4ea6ed21c4a11a3388
Nikita <lemper2015@gmail.com>
2019-05-02 19:07:06 +0300

    debug registration

--- backend/connection.php
+++ backend/connection.php
@@ -9,5 +9,7 @@ $password = 'sLWV6ltE';
 
 $link = mysqli_connect($host, $user, $password, $database)
 or die("Ошибка " . mysqli_error($link));
+mysqli_query($link, "SET NAMES 'utf8' COLLATE 'utf8_general_ci'");
+mysqli_query($link, "SET CHARACTER SET 'utf8'");
 //reg
 ?>
